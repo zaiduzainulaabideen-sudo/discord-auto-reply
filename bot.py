ade3d776781475077436262b4212eb40571430b7
zaiduzainulaabideen-sudo <zaiduzainulaabideen@gmail.com>
2026-08-17 17:41:05 +0530

    Create bot.py
    Hii everyone

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot is online as {bot.user}")
+
+
+@bot.event
+async def on_message(message):
+    # Don't reply to itself
+    if message.author == bot.user:
+        return
+
+    msg = message.content.lower().strip()
+
+    # Auto-reply
+    if msg in ["hi", "hii", "hello", "hey", "help"]:
+        await message.channel.send(
+            "Hii! I'm Zaidu Sir's bot 🤖\n"
+            "I'm here to help you with anything related to our game, Street Owners.\n"
+            "How may I help you?"
+        )
+
+    await bot.process_commands(message)
+
+
+bot.run("YOUR_BOT_TOKEN")
